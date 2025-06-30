37af00656b7b08b85459b7aaf3f69faa7a81812a
NoVaOsiris <95081024+NoVaOsiris@users.noreply.github.com>
2025-06-30 18:19:41 +0300

    Delete server directory

--- server/models/Sale.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const SaleSchema = new mongoose.Schema({
-  seller: String,
-  point: String,
-  items: Object,
-  timestamp: { type: Date, default: Date.now }
-});
-module.exports = mongoose.model("Sale", SaleSchema);
--- server/models/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const UserSchema = new mongoose.Schema({
-  name: String,
-  password: String,
-  role: { type: String, enum: ["admin", "seller"], default: "seller" },
-  point: String
-});
-module.exports = mongoose.model("User", UserSchema);
